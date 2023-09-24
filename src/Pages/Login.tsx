5ab3224655be3c28f3c66796de41842f7be7841a
Sachin Ingole <sachin.ingole@codecraft.co.in>
2023-09-25 02:37:37 +0530

    feat: created signup page

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import styled from 'styled-components';
 
 const Login = () => {
-  return <div>Login</div>;
+  return <Warapper></Warapper>;
 };
+
+const Warapper = styled.div``;
 
 export default Login;
