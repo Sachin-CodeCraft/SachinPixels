fda9f2522081eb78e4488361b553bbabe4ffb603
Sachin-CodeCraft <sachin.ingole@codecraft.co.in>
2023-11-06 10:55:42 +0530

    fix: routing added

--- src/Pages/Login.tsx
+++ src/Pages/Login.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Login = () => {
-  return <Warapper></Warapper>;
+  return (
+    <Warapper>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis quis
+      nihil id voluptatibus iste a numquam ducimus labore cumque. In molestiae
+      et qui distinctio culpa odit nesciunt id dignissimos voluptatem.
+    </Warapper>
+  );
 };
 
 const Warapper = styled.div``;
